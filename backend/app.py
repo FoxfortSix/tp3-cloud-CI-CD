463567157029f34e15ad6bda831025d540dec3c5
Mochammad Azka Basria <pindandss2@gmail.com>
2026-05-06 11:20:11 +0700

    initial commit

---
+++ backend/app.py
@@ -0,0 +1,35 @@
+import os
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+CORS(app)
+
+# Mengambil Nama dan NIM dari environment variables
+user_name = os.getenv('USER_NAME', 'Nama Default')
+user_nim = os.getenv('USER_NIM', 'NIM Default')
+
+kantin_data = {
+    "nama_kantin": "Kantin FPMIPA",
+    "nama_mahasiswa": user_name,
+    "nim_mahasiswa": user_nim,
+    "menu": ["Nasi Goreng", "Es Teh", "Gorengan"]
+}
+
+
+@app.route('/api/info', methods=['GET'])
+def get_info():
+    return jsonify(kantin_data)
+
+
+@app.route('/api/add-menu', methods=['POST'])
+def add_menu():
+    new_item = request.json.get('item')
+    if new_item:
+        kantin_data["menu"].append(new_item)
+        return jsonify({"message": "Menu berhasil ditambah!", "menu": kantin_data["menu"]}), 201
+    return jsonify({"error": "Data tidak valid"}), 400
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
